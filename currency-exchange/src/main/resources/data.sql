d1de4308390a76a130d81d43462e04a46f01d05d
Saumya Sinha <saumya.sinha@newgensoft.com>
2026-05-12 02:29:10 +0530

    Pushing microservice workspace

---
+++ currency-exchange/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into CURRENCY_EXCHANGE (id,currency_from,currency_to,conversion_rate) values (1,'USD','INR',100);
+
+insert into CURRENCY_EXCHANGE (id,currency_from,currency_to,conversion_rate) values (2,'YEN','INR',75);
+insert into CURRENCY_EXCHANGE (id,currency_from,currency_to,conversion_rate) values (3,'EUR','INR',80);
+insert into CURRENCY_EXCHANGE (id,currency_from,currency_to,conversion_rate) values (4,'AUD','INR',90);
